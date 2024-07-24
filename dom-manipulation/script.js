9b7a7585a7a3a3237e18202771fc9f7a6f559659
Vallor23 <enosnyabinda2017@gmail.com>
2024-07-24 21:21:05 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -8,8 +8,6 @@ let quotes =[{
 
 //Get  reference for DOM elements
 const showNewQuote = document.getElementById("newQuote");
-const newQuoteText = document.getElementById('newQuoteText').value;
-const newQuoteCategory = document.getElementById('newQuoteCategory').value;
 
 // Function to display a random quote
 function showRandomQuote(){
@@ -18,8 +16,34 @@ function showRandomQuote(){
     quoteDisplay.innerHTML = quotes[randomIndex].text;
     quoteDisplay.style.background = 'rgb(145, 213, 240)';
 }
+
+// Function to create the form for adding new quotes
+function createAddQuoteForm() {
+    const formContainer = document.createElement('div');
+    const quoteInput = document.createElement('input');
+    quoteInput.id ='newQuoteText';
+    quoteInput.text = 'text';
+    quoteInput.placeholder = 'Enter a new quote';
+
+    const categoryInput = document.createElement('input');
+    categoryInput.id ='newQuoteCategory';
+    categoryInput.text = 'text';
+    categoryInput.placeholder = 'Enter quote category';
+
+    const addButton = document.createElement('button');
+    addButton.onclick = addQuote;
+    addButton.textContent = 'AddQuote';
+
+    formContainer.appendChild(quoteInput);
+    formContainer.appendChild(categoryInput);
+    formContainer.appendChild(addButton);
+
+    document.body.appendChild(formContainer);
+}
 // Function to add a new quote
 function addQuote() {
+    const newQuoteText = document.getElementById('newQuoteText').value;
+    const newQuoteCategory = document.getElementById('newQuoteCategory').value;
     if(newQuoteText && newQuoteCategory){
         quotes.push({text: newQuoteText, category: newQuoteCategory});
         newQuoteText = '';
@@ -28,5 +52,8 @@ function addQuote() {
         alert('Please enter both quote text and category')
     }
 }
+
 // Event listener for showing a new quote
 showNewQuote.addEventListener('click', showRandomQuote);
+// Call the function to create the form on page load
+createAddQuoteForm();
